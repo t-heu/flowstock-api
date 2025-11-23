8d90b89f1e5ef3988c0c5d6d80589db51d94dcc0
t-heu <teuzin375@gmail.com>
2025-11-23 14:44:15 -0300

    fix: prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,11 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+//import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-// Evita múltiplas instâncias no hot reload
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const connectionString = `${process.env.DATABASE_URL}`
 
-const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
-export const prisma =
-  global.prisma ?? new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export { prisma }
