84c486dce94255cdb57fe7815a27439600b40220
t-heu <teuzin375@gmail.com>
2025-11-23 17:03:01 -0300

    prisma att

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,10 @@
-// src/prismaClient.ts
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
