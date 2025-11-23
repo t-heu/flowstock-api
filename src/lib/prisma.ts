756a44408f313378823e98a25b58f7e97a035720
t-heu <teuzin375@gmail.com>
2025-11-23 16:58:02 -0300

    prisma v22

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,8 @@
+// src/prismaClient.ts
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ log: ['query'] });
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
