004bab475f4bc40ebf251a29282a0eb7d219fb5f
t-heu <teuzin375@gmail.com>
2025-12-31 11:28:26 -0300

    fix: config

--- src/core/prisma/client.ts
+++ src/core/prisma/client.ts
@@ -3,6 +3,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`
+if (!connectionString) throw new Error("DATABASE_URL não configurado");
 
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
